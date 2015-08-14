a224e4f0932f5ad92b5eba50ef2056e51b6df1c8
AyeSandaAung <e0001341@u.nus.edu>
2015-08-14 17:43:57 +0800

    Finish Ex01-08

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -9,8 +9,18 @@ namespace CSExercises
     public class Ex07
     {
         public static void Main(string[] args)
-        {
-            
+        {          
+            Console.WriteLine("Please enter your salary : ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            double housingAllowance = salary * 0.1; //10%
+           // Console.WriteLine("hallow   :   "+housingAllowance );
+
+            double transportAllowance = salary * 0.03;  //3%
+           // Console.WriteLine("transAllow   :  "+ transportAllowance);
+
+            Console.WriteLine("The total income is : {0:c}", salary+housingAllowance+transportAllowance);
+
         }
     }
 }
--- CSExercises/Ex07-15/Ex08.cs
+++ CSExercises/Ex07-15/Ex08.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter a temperature : ");
+            int temperature = Convert.ToInt32(Console.ReadLine());
+
+            // F=1.8C + 32;           
+
+            Console.WriteLine("The Temperature in Fahrenheit is : {0}", (1.8 * temperature) + 32);
         }
     }
 }
